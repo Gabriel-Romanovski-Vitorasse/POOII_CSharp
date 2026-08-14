cf45f9139fa62b2cfa417ddf326b43d079578047
Gabriel Romanovski Vitorasse <vitorassegabriel@gmail.com>
2026-08-14 14:39:43 -0300

    Adição do exercicio um

---
+++ Aula20260814_Exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int idade;
+        float total = 0, qtdeA = 0, qtdeB = 0, qtdeC = 0, qtdeD = 0, qtdeE = 0;
+
+        Console.WriteLine("========================================");
+        Console.WriteLine("Gestão de Pessoas");
+        Console.WriteLine("========================================");
+
+        do { } while ();
+        Console.WriteLine("Digite a idade:");
+        idade = int.Parse(Console.ReadLine());
+
+
+
+    }
+}
