686e06b2da50c0378940eda7549ce4091e744100
Gabriel Romanovski Vitorasse <vitorassegabriel@gmail.com>
2026-08-18 08:49:44 -0300

    Edição - Facul
    Terminar em casa para entregar

---
+++ Aula20260818/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Digite seu nome";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(222, 21);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(271, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Digite seu sobrenome";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(222, 74);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(271, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(392, 108);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Clique aqui";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Form1_Load;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(48, 215);
+            label3.Name = "label3";
+            label3.Size = new Size(126, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Seu nome completo é:";
+            label3.Click += label3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(222, 207);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(271, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(26, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(609, 256);
+            panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(666, 291);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox3;
+        private Panel panel1;
+    }
+}
