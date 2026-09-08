af8e834be4047421a5877e84c0959787f3006b84
Gabriel Romanovski Vitorasse <vitorassegabriel@gmail.com>
2026-09-08 08:53:37 -0300

    Add files via upload

---
+++ IdeiasInovadoras/control/IdeiasInovadoraControle.cs
@@ -0,0 +1,33 @@
+﻿using IdeiasInovadoras.model;
+using IdeiasInovadoras.persistencia;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Ink;
+
+namespace IdeiasInovadoras.control
+{
+    internal class IdeiasInovadoraControle
+    {
+
+        private IdeiaInovadora modeloii = new();
+
+        public Boolean ControlCadastrarII(String area, string ideia, float custo)
+        {
+
+            ideia = ideia + "!!!!!!";
+
+            IdeiaInovadora ii = new()
+            {
+                Area = area,
+                Ideia = ideia,
+                Custo = custo
+            };
+
+            if (modeloii.CadastrarII(ii))
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ IdeiasInovadoras/persistencia/BD.cs
@@ -0,0 +1,16 @@
+﻿using IdeiasInovadoras.model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdeiasInovadoras.persistencia
+{
+    internal class BD
+    {
+        public static List<IdeiaInovadora> mybd = new();
+
+        public static void SalvarBD(IdeiaInovadora ii) => mybd.Add(ii);
+
+        public static List<IdeiaInovadora> RetornarBD() => mybd;
+    }
+}
